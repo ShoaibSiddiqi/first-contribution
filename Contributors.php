3bab53d553eb3111313e839f1878996f51e26b6f
Nida-Batool <nidabatoolkhan@gmail.com>
2018-10-29 11:35:59 +0500

    add nida khan to contributors list

---
+++ Contributors.php
@@ -0,0 +1,5 @@
+# Contributors
+
+- [Shoaib Siddiqi](https://github.com/ShoaibSiddiqi)
+- [Nida-khan](https://github.com/Nida-Batool/first-contribution.git)
+- [Muhammad Ali ](https://github.com/designsbyali)
